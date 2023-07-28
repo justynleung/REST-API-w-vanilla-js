04cb574d064016d405967982c37557b170751fc8
justynleung <justyn@Justyns-MacBook-Pro.local>
2023-07-27 23:10:53 -0400

    title to name

--- controller/productController.js
+++ controller/productController.js
@@ -32,10 +32,10 @@ const createProduct = async (req, res) => {
     try {
         const body = await getPostData(req)
 
-        const { title, description, price } = JSON.parse(body)
+        const { name, description, price } = JSON.parse(body)
 
         const product = {
-            title,
+            name,
             description,
             price
         }
